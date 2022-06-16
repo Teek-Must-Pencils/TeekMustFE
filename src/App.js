ed31a76155637b6ea521d6d19a410de9acb96d35
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-06-16 18:30:49 +0700

    Update Route & Pages
    - make Auth Route Protection
    - make Pages Product Page\
    - Add Image dummy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Layouts, Login } from './Pages';
+import { Home, Layouts, Login, ProductPage } from './Pages';
 import './App.css';
 import Register from './Pages/Register/Register';
+import AuthRoute from './AuthRoute/AuthRoute';
 
+const LayoutsAuth = ({children}) =>{
+  return(
+    <AuthRoute>
+      <Layouts>
+        {children}
+      </Layouts>
+    </AuthRoute>
+  )
+}
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layouts><Home/></Layouts>}/>
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
+      <Route 
+        path='/' 
+        element={<LayoutsAuth><Home/></LayoutsAuth>}
+      />
+      <Route 
+        path='/productPage' 
+        element={<LayoutsAuth><ProductPage /></LayoutsAuth>}
+      />
     </Routes>
   );
 }
