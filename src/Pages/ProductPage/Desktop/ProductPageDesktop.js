0d407865406dbce4e06b99baa9bd547092caeb51
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-06-20 14:42:59 +0700

    Update Clear comment

--- src/Pages/ProductPage/Desktop/ProductPageDesktop.js
+++ src/Pages/ProductPage/Desktop/ProductPageDesktop.js
@@ -82,6 +82,7 @@ const ProductPageDesktop = (props) => {
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     }
     
+    // console.log(dataPreview.imageFile)
         
   return (
     <>
