ec46937ac51cfb7cc6166dd20594d765f3d8f734
indrasatriaxx <indrapanca.xx@gmail.com>
2022-07-01 21:20:52 +0700

    Update ServiecProfile.js

--- src/Services/ServiecProfile.js
+++ src/Services/ServiecProfile.js
@@ -1,41 +1,49 @@
 import axios from 'axios';
 
-const ServiecProfile = {
+const ServiceProfile = {
 
-  async getUserByEmail(email){
+  async UpdateProfile(value) {
     const sessionData = sessionStorage.getItem('user')
-    const dt = JSON.parse(sessionData);
+    const dt = JSON.parse(sessionData)
     const token = dt.accessToken
-    const data = await axios({
-        method: 'get',
-        url: process.env.REACT_APP_BASE_URL+`api/product/${email}`,
-        headers:{
-            "Authorization" : `Bearer ${token}`
-        }
-      })
-      .then((response) => response)
-      .catch((err) => err.response)
-   
-      return data
-  },
+    let FormData = require('form-data');
+    let dataSend = new FormData();
+    dataSend.append('id', value.id)
+    dataSend.append('address', value.address)
+    dataSend.append('number', value.number)
 
-  async EditUser(email, value){
-    const sessionData = sessionStorage.getItem('user')
-    const dt = JSON.parse(sessionData);
-    const token = dt.accessToken
     const data = await axios({
-        method: 'POST',
-        url: process.env.REACT_APP_BASE_URL+`api/product/${email}`,
-        headers:{
-            "Authorization" : `Bearer ${token}`
-        },
-        data: value
-      })
-      .then((response) => response)
-      .catch((err) => err.response)
-   
-      return data
+      method: 'PUT',
+      url: process.env.REACT_APP_BASE_URL+`api/profile`,
+      headers : {
+        'Authorization' : `Bearer ${token}`,
+      },
+      data: dataSend
+    })
+    .then((response) => response)
+    .catch((err) => err.response)
+
+    return data
+
   }
+
+  // async EditUser(email, value){
+  //   const sessionData = sessionStorage.getItem('user')
+  //   const dt = JSON.parse(sessionData);
+  //   const token = dt.accessToken
+  //   const data = await axios({
+  //       method: 'POST',
+  //       url: process.env.REACT_APP_BASE_URL+`api/product/${email}`,
+  //       headers:{
+  //           "Authorization" : `Bearer ${token}`
+  //       },
+  //       data: value
+  //     })
+  //     .then((response) => response)
+  //     .catch((err) => err.response)
+   
+  //     return data
+  // }
 }
 
-export default ServiecProfile
+export default ServiceProfile
