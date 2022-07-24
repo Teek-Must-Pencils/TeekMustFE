7d2024295cc0b7594029397cb6c5f0964584aa88
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-07-24 11:45:24 +0700

    Update InfoProfile
    - ada action Edit

--- src/Pages/InfoProfile/InfoProfileMobile/InfoProfileMobile.js
+++ src/Pages/InfoProfile/InfoProfileMobile/InfoProfileMobile.js
@@ -7,19 +7,39 @@ import "./InfoProfileMobile.css"
 
 // Gambar
 import ServiceProfile from "../../../Services/ServiecProfile";
+import { Loading, ModalNotification } from "../../../Components";
 
 
 const InfoProfileMobile = (props) => {
     const { userData }= props; 
-    console.log('test',userData)
     const navigate = useNavigate();
-    const [image, setImage] = useState(userData?.img);
     const { register, handleSubmit, setValue } = useForm();
+    const [image, setImage] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [isNotification, setIsNotification] = useState(false);
+    const [message, setMessage] = useState();
     
-    const onSubmit = data => {
-        ServiceProfile.UpdateProfile(data)
-        .then((res) => console.log(res))
-        console.log(data);
+    const onSubmitEdit = data => {
+        setIsLoading(true);
+        ServiceProfile.UpdateProfile(data).then((res) => {
+            if (res.status === 200) {
+                setMessage('Edit Data Berhasil')
+                setIsLoading(false);
+                setIsNotification(true);
+                setTimeout(() => {
+                    setIsNotification(false);
+                    setMessage('');
+                    window.location.reload();
+                }, 1000);
+            } else {
+                setMessage('Edit Data Gagal')
+                setIsLoading(false);
+                setIsNotification(true);
+                setTimeout(() => {
+                    setIsNotification(false);
+                    setMessage('');
+                }, 1000);}
+        }).catch((err) => console.log(err))
     }
 
     const handleImage = (e) =>{
@@ -28,7 +48,7 @@ const InfoProfileMobile = (props) => {
         reader.onload = () => {
             if (reader.readyState === 2) {
                 setImage(reader.result);
-                // setValue("image", reader.result)
+                setValue("image", reader.result)
             }
         };
         reader.readAsDataURL(e.target.files[0]);
@@ -38,86 +58,91 @@ const InfoProfileMobile = (props) => {
         navigate(-1)
     }
 
+    setValue('name', userData?.username);
+    setValue('address', userData?.address);
+    setValue('number', userData?.number);
+    setValue('id', userData?.id);
+    setValue('image', `data:image/png;base64,${userData?.imgB}`);
+
     return (
-        <div className="container-content">
+        <>
+        <Loading show={isLoading}/>
+        <ModalNotification show={isNotification} message={message} />
+            <div className="container-content">
 
-             <div className="button-back-content">
-                <button 
-                    className=''
-                    onClick={()=> handleBack()}
-                >
-                    <ArrowLeft size='20px'/>
-                </button>
+                <div className="button-back-content">
+                    <button 
+                        className=''
+                        onClick={()=> handleBack()}
+                    >
+                        <ArrowLeft size='20px'/>
+                    </button>
+                </div>
+        
+                <div className="container">
+                    <Row className="form-profile justify-content-center align-items-center">
+                        <div className="col-11">
+                            <form onSubmit={handleSubmit(onSubmitEdit)} className='' >
+                            <input type='hidden' defaultValue='' {...register("id")} />
+                                <div className="d-flex flex-row gap-4 my-5">
+                                    <Image className="mx-auto d-block mb-3 if-img-mobile" src={image} />
+                                    <Form.Group controlId="formFile" className="mb-3">
+                                        <Form.Label>Foto*</Form.Label>
+                                        <Form.Control size="sm" type="file" onChange={e => handleImage(e)} />
+                                    </Form.Group> 
+                                </div>
+                                <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>Nama*</Form.Label>
+                                    <Form.Control {...register("name")}
+                                        size="sm"
+                                        type="text"
+                                        placeholder="Nama" 
+                                        defaultValue={userData?.username}
+                                    />
+                                </Form.Group>
+
+                                {/* <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>Kota*</Form.Label>
+                                    <Form.Select size="lg" {...register("Kota")}>
+                                        <option>Pilih Kota</option>
+                                        <option>Jabodetabek</option>
+                                        <option>Bali</option>
+                                        <option>Luar Jawa</option>
+                                    </Form.Select>
+                                </Form.Group> */}
+
+                                <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>Alamat</Form.Label>
+                                    <Form.Control {...register("address")}
+                                        as="textarea"
+                                        rows={3}
+                                        size="sm"
+                                        type="text"
+                                        placeholder="Alamat" 
+                                        defaultValue={userData?.address}
+                                    />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>No Handphone*</Form.Label>
+                                    <Form.Control {...register("number")}
+                                        size="sm"
+                                        type="number"
+                                        placeholder="contoh: 08123456789" 
+                                        defaultValue={userData?.number}
+                                    />
+                                </Form.Group>
+
+                                <button className="tombol-simpan-mobile">
+                                    Simpan
+                                </button>
+                            </form> 
+
+                        </div>
+                    </Row>
+                </div>
             </div>
-    
-            <div className="container">
-                <Row className="form-profile justify-content-center align-items-center">
-                    <div className="col-11">
-                        <form onSubmit={handleSubmit(onSubmit)} className='' >
-                        <input type='hidden' defaultValue='' {...register("id")} />
-                            <div className="d-flex flex-row gap-4 my-5">
-                                <Image className="mx-auto d-block mb-3 if-img-mobile" src={image} />
-                                <Form.Group controlId="formFile" className="mb-3">
-                                    <Form.Label>Foto*</Form.Label>
-                                    <Form.Control size="sm" type="file" onChange={e => handleImage(e)} />
-                                </Form.Group> 
-                            </div>
-                            <Form.Group className="mb-3" controlId="">
-                                <Form.Label>Nama*</Form.Label>
-                                <Form.Control {...register("name")}
-                                    size="sm"
-                                    type="text"
-                                    placeholder="Nama" 
-                                    defaultValue={userData?.username}
-                                />
-                            </Form.Group>
-
-                            {/* <Form.Group className="mb-3" controlId="">
-                                <Form.Label>Kota*</Form.Label>
-                                <Form.Select size="lg" {...register("Kota")}>
-                                    <option>Pilih Kota</option>
-                                    <option>Jabodetabek</option>
-                                    <option>Bali</option>
-                                    <option>Luar Jawa</option>
-                                </Form.Select>
-                            </Form.Group> */}
-
-                            <Form.Group className="mb-3" controlId="">
-                                <Form.Label>Alamat</Form.Label>
-                                <Form.Control {...register("address")}
-                                    as="textarea"
-                                    rows={3}
-                                    size="sm"
-                                    type="text"
-                                    placeholder="Alamat" 
-                                    defaultValue={userData?.address}
-                                />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="">
-                                <Form.Label>No Handphone*</Form.Label>
-                                <Form.Control {...register("number")}
-                                    size="sm"
-                                    type="number"
-                                    placeholder="contoh: 08123456789" 
-                                    defaultValue={userData?.number}
-                                />
-                            </Form.Group>
-
-                            <button className="tombol-simpan-mobile">
-                                Simpan
-                            </button>
-                        </form> 
-
-                    </div>
-                </Row>
-               
-            </div>
-
-          
-
-
-        </div>
+        </>
     )
 }
 
--- src/Pages/InfoProfile/InfoprofileDesktop/InfoProfileDesktop.js
+++ src/Pages/InfoProfile/InfoprofileDesktop/InfoProfileDesktop.js
@@ -7,18 +7,39 @@ import "./InfoProfileDesktop.css"
 
 // Gambar
 import ServiceProfile from "../../../Services/ServiecProfile";
+import { Loading, ModalNotification } from "../../../Components";
 
 
 const InfoProfileDesktop = (props) => {
     const { userData } = props;
     const navigate = useNavigate();
-    const [image, setImage] = useState();
     const { register, handleSubmit, setValue } = useForm();
+    const [image, setImage] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [isNotification, setIsNotification] = useState(false);
+    const [message, setMessage] = useState();
 
-    const onSubmit = data => {
-        ServiceProfile.UpdateProfile(data)
-            .then((res) => console.log(res))
-        console.log(data);
+    const onSubmitEdit = data => {
+        setIsLoading(true);
+        ServiceProfile.UpdateProfile(data).then((res) => {
+            if (res.status === 200) {
+                setMessage('Edit Data Berhasil')
+                setIsLoading(false);
+                setIsNotification(true);
+                setTimeout(() => {
+                    setIsNotification(false);
+                    setMessage('');
+                    window.location.reload();
+                }, 1000);
+            } else {
+                setMessage('Edit Data Gagal')
+                setIsLoading(false);
+                setIsNotification(true);
+                setTimeout(() => {
+                    setIsNotification(false);
+                    setMessage('');
+                }, 1000);}
+        }).catch((err) => console.log(err))
     }
 
     const handleImage = (e) => {
@@ -27,7 +48,7 @@ const InfoProfileDesktop = (props) => {
         reader.onload = () => {
             if (reader.readyState === 2) {
                 setImage(reader.result);
-                // setValue("image", reader.result)
+                setValue("image", reader.result)
             }
         };
         reader.readAsDataURL(e.target.files[0]);
@@ -37,78 +58,82 @@ const InfoProfileDesktop = (props) => {
         navigate(-1)
     }
 
-    console.log(userData?.imgB)
+    setValue('name', userData?.username);
+    setValue('address', userData?.address);
+    setValue('number', userData?.number);
+    setValue('id', userData?.id);
+    setValue('image', `data:image/png;base64,${userData?.imgB}`);
+    
 
     return (
-        <div className="container-content">
+        <>
+        <Loading show={isLoading}/>
+        <ModalNotification show={isNotification} message={message} />
+            <div className="container-content">
 
-            <div className="button-back-content-df">
-                <button
-                    className=''
-                    onClick={() => handleBack()}
-                >
-                    <ArrowLeft size='20px' />
-                </button>
-            </div>
+                <div className="button-back-content-df">
+                    <button
+                        className=''
+                        onClick={() => handleBack()}
+                    >
+                        <ArrowLeft size='20px' />
+                    </button>
+                </div>
 
-            <div className="container">
-                <Row className="form-profile justify-content-center align-items-center">
-                    <div className="col-6">
-                        <Form onSubmit={handleSubmit(onSubmit)} className={'form-login'} >
-                            <input type='hidden' defaultValue='' {...register("id")} />
-                            <div className="d-flex flex-row gap-4">
-                                <Image className="mx-auto d-block mb-3 if-img" src={image || `data:image/png;base64,${userData?.imgB}`} />
-                                <Form.Group controlId="formFile" className="mb-3">
-                                    <Form.Label>Foto*</Form.Label>
-                                    <Form.Control type="file" onChange={e => handleImage(e)} />
+                <div className="container">
+                    <Row className="form-profile justify-content-center align-items-center">
+                        <div className="col-6">
+                            <Form onSubmit={handleSubmit(onSubmitEdit)} className={'form-login'} >
+                                <input type='hidden' defaultValue='' {...register("id")} />
+                                <div className="d-flex flex-row gap-4">
+                                    <Image className="mx-auto d-block mb-3 if-img" src={image || `data:image/png;base64,${userData?.imgB}`} />
+                                    <Form.Group controlId="formFile" className="mb-3">
+                                        <Form.Label>Foto*</Form.Label>
+                                        <Form.Control type="file" onChange={e => handleImage(e)} />
+                                    </Form.Group>
+                                </div>
+
+                                <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>Username*</Form.Label>
+                                    <Form.Control {...register("name")}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Username"
+                                        // defaultValue={userData?.username}
+                                    />
                                 </Form.Group>
-                            </div>
 
-                            <Form.Group className="mb-3" controlId="">
-                                <Form.Label>Username*</Form.Label>
-                                <Form.Control {...register("name")}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Username"
-                                    defaultValue={userData?.username}
-                                />
-                            </Form.Group>
+                                <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>Alamat</Form.Label>
+                                    <Form.Control {...register("address")}
+                                        as="textarea"
+                                        rows={3}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Alamat"
+                                        // defaultValue={userData?.address}
+                                    />
+                                </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="">
-                                <Form.Label>Alamat</Form.Label>
-                                <Form.Control {...register("address")}
-                                    as="textarea"
-                                    rows={3}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Alamat"
-                                    defaultValue={userData?.address}
-                                />
-                            </Form.Group>
+                                <Form.Group className="mb-3" controlId="">
+                                    <Form.Label>No Handphone*</Form.Label>
+                                    <Form.Control {...register("number")}
+                                        size="lg"
+                                        type="number"
+                                        placeholder="contoh: +628123456789"
+                                        // defaultValue={userData?.number}
+                                    />
+                                </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="">
-                                <Form.Label>No Handphone*</Form.Label>
-                                <Form.Control {...register("number")}
-                                    size="lg"
-                                    type="number"
-                                    placeholder="contoh: +628123456789"
-                                    defaultValue={userData?.number}
-                                />
-                            </Form.Group>
-
-                            <button className="tombol-simpan">
-                                Simpan
-                            </button>
-                        </Form>
-
-
-                    </div>
-                </Row>
+                                <button className="tombol-simpan mb-5" type="submit">
+                                    Simpan
+                                </button>
+                            </Form>
+                        </div>
+                    </Row>
+                </div>
             </div>
-
-
-
-        </div>
+        </>
     )
 }
 
